650db2237e41fcc94333011d7918d04cd20d30c0
rezimOne <michalreda@gmail.com>
2020-09-25 08:06:33 +0200

    refactor(app module)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,26 @@
+import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavigationBarComponent } from './core/navigation-bar/navigation-bar.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FooterComponent } from './core/footer/footer.component';
-import { ContainerComponent } from './core/container/container.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavigationBarComponent,
-    FooterComponent,
-    ContainerComponent
+    AppComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    CoreModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [AppComponent],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
